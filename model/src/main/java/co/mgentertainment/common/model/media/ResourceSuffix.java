c324268c35126244eafcd6ddc2e157a51c1e5315
larry <larryland000@gmail.com>
2023-09-24 14:11:20 +0400

    update

---
+++ model/src/main/java/co/mgentertainment/common/model/media/ResourceSuffix.java
@@ -0,0 +1,13 @@
+package co.mgentertainment.common.model.media;
+
+/**
+ * @author larry
+ * @createTime 2023/9/24
+ * @description ResourceSuffix
+ */
+public interface ResourceSuffix {
+    String FILM_DIR = "_hls";
+    String TRAILER_DIR = "_trailer";
+    String FEATURE_FILM = ".m3u8";
+    String TRAILER = ".trailer.mp4";
+}
