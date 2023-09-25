483972984b1030f6b7eacd885d79e1026063dc34
larry <larryland000@gmail.com>
2023-09-25 19:51:15 +0400

    update

--- common-syslog-spring-boot-starter/src/main/java/co/mgentertainment/common/syslog/aspect/SysLogAspect.java
+++ common-syslog-spring-boot-starter/src/main/java/co/mgentertainment/common/syslog/aspect/SysLogAspect.java
@@ -22,62 +22,59 @@ import org.springframework.expression.EvaluationContext;
 @Slf4j
 public class SysLogAspect {
 
-	@Around("@annotation(sysLog)")
-	@SneakyThrows
-	public Object around(ProceedingJoinPoint point, SysLog sysLog) {
-		String strClassName = point.getTarget().getClass().getName();
-		String strMethodName = point.getSignature().getName();
-		log.debug("[类名]:{},[方法]:{}", strClassName, strMethodName);
+    @Around("@annotation(sysLog)")
+    @SneakyThrows
+    public Object around(ProceedingJoinPoint point, SysLog sysLog) {
+        String strClassName = point.getTarget().getClass().getName();
+        String strMethodName = point.getSignature().getName();
+        log.debug("[类名]:{},[方法]:{}", strClassName, strMethodName);
 
-		String value = sysLog.value();
-		String expression = sysLog.expression();
-		// 当前表达式存在 SPEL，会覆盖 value 的值
-		if (StringUtils.isNotBlank(expression)) {
-			// 解析SPEL
-			MethodSignature signature = (MethodSignature) point.getSignature();
-			EvaluationContext context = SysLogUtils.getContext(point.getArgs(), signature.getMethod());
-			try {
-				value = SysLogUtils.getValue(context, expression, String.class);
-			}
-			catch (Exception e) {
-				// SPEL 表达式异常，获取 value 的值
-				log.error("@SysLog 解析SPEL {} 异常", expression);
-			}
-		}
-		String[] split = value.split("_");
-		SysLogDO logVo = SysLogUtils.getSysLog();
-		if (split.length==3){
-			String oprationModule = split[0];
-			String oprationText = split[1];
-			String oprationtType = split[2];
+        String value = sysLog.value();
+        String expression = sysLog.expression();
+        // 当前表达式存在 SPEL，会覆盖 value 的值
+        if (StringUtils.isNotBlank(expression)) {
+            // 解析SPEL
+            MethodSignature signature = (MethodSignature) point.getSignature();
+            EvaluationContext context = SysLogUtils.getContext(point.getArgs(), signature.getMethod());
+            try {
+                value = SysLogUtils.getValue(context, expression, String.class);
+            } catch (Exception e) {
+                // SPEL 表达式异常，获取 value 的值
+                log.error("@SysLog 解析SPEL {} 异常", expression);
+            }
+        }
+        String[] split = value.split("_");
+        SysLogDO logVo = SysLogUtils.getSysLog(point.getArgs());
+        if (split.length == 3) {
+            String oprationModule = split[0];
+            String oprationText = split[1];
+            String oprationtType = split[2];
 
-			logVo.setOprationModule(oprationModule);
-			logVo.setOprationText(oprationText);
-			logVo.setOprationType(oprationtType);
-		}
-		logVo.setTitle(value);
+            logVo.setOprationModule(oprationModule);
+            logVo.setOprationText(oprationText);
+            logVo.setOprationType(oprationtType);
+        }
+        logVo.setTitle(value);
 
 
-		// 发送异步日志事件
-		Long startTime = System.currentTimeMillis();
-		Object obj;
+        // 发送异步日志事件
+        Long startTime = System.currentTimeMillis();
+        Object obj;
 
-		try {
-			obj = point.proceed();
-		}
-		catch (Exception e) {
-			logVo.setType(LogTypeEnum.ERROR.getType());
-			logVo.setException(e.getMessage());
-			throw e;
-		}
-		finally {
-			Long endTime = System.currentTimeMillis();
-			logVo.setTime(endTime - startTime);
-			logVo.setServiceId(System.getProperty("app.name",""));
-			SpringContextHolder.publishEvent(new SysLogEvent(logVo));
-		}
+        try {
+            obj = point.proceed();
+        } catch (Exception e) {
+            logVo.setType(LogTypeEnum.ERROR.getType());
+            logVo.setException(e.getMessage());
+            throw e;
+        } finally {
+            Long endTime = System.currentTimeMillis();
+            logVo.setTime(endTime - startTime);
+            logVo.setServiceId(System.getProperty("app.name", ""));
+            SpringContextHolder.publishEvent(new SysLogEvent(logVo));
+        }
 
-		return obj;
-	}
+        return obj;
+    }
 
 }
--- common-syslog-spring-boot-starter/src/main/java/co/mgentertainment/common/syslog/util/SysLogUtils.java
+++ common-syslog-spring-boot-starter/src/main/java/co/mgentertainment/common/syslog/util/SysLogUtils.java
@@ -4,7 +4,9 @@ import cn.hutool.core.util.URLUtil;
 import cn.hutool.extra.servlet.ServletUtil;
 import cn.hutool.http.HttpUtil;
 import co.mgentertainment.common.syslog.dal.po.SysLogDO;
+import co.mgentertainment.common.utils.GsonFactory;
 import lombok.experimental.UtilityClass;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.core.LocalVariableTableParameterNameDiscoverer;
 import org.springframework.expression.EvaluationContext;
 import org.springframework.expression.Expression;
@@ -28,64 +30,68 @@ import java.util.Objects;
 @UtilityClass
 public class SysLogUtils {
 
-	public SysLogDO getSysLog() {
-		HttpServletRequest request = ((ServletRequestAttributes) Objects
-			.requireNonNull(RequestContextHolder.getRequestAttributes())).getRequest();
-		SysLogDO sysLog = new SysLogDO();
-		sysLog.setType(LogTypeEnum.NORMAL.getType());
-		sysLog.setRemoteAddr(ServletUtil.getClientIP(request));
-		sysLog.setRequestUri(URLUtil.getPath(request.getRequestURI()));
-		sysLog.setMethod(request.getMethod());
-		sysLog.setUserAgent(request.getHeader(HttpHeaders.USER_AGENT));
-		sysLog.setParams(HttpUtil.toParams(request.getParameterMap()));
-		sysLog.setCreateBy(getUsername());
-		sysLog.setUpdateBy(getUsername());
-		return sysLog;
-	}
+    public SysLogDO getSysLog(Object[] args) {
+        HttpServletRequest request = ((ServletRequestAttributes) Objects
+                .requireNonNull(RequestContextHolder.getRequestAttributes())).getRequest();
+        SysLogDO sysLog = new SysLogDO();
+        sysLog.setType(LogTypeEnum.NORMAL.getType());
+        sysLog.setRemoteAddr(ServletUtil.getClientIP(request));
+        sysLog.setRequestUri(URLUtil.getPath(request.getRequestURI()));
+        sysLog.setMethod(request.getMethod());
+        sysLog.setUserAgent(request.getHeader(HttpHeaders.USER_AGENT));
+        sysLog.setParams(StringUtils.equalsIgnoreCase(request.getMethod(), "post") ? GsonFactory.getGson().toJson(args) :
+                HttpUtil.toParams(request.getParameterMap()));
+        sysLog.setCreateBy(getUsername());
+        sysLog.setUpdateBy(getUsername());
+        return sysLog;
+    }
 
-	/**
-	 * 获取用户名称
-	 * @return username
-	 */
-	private String getUsername() {
-		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-		if (authentication == null) {
-			return null;
-		}
-		User user = (User) authentication.getPrincipal();
-		return user.getUsername();
-	}
+    /**
+     * 获取用户名称
+     *
+     * @return username
+     */
+    private String getUsername() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication == null) {
+            return null;
+        }
+        User user = (User) authentication.getPrincipal();
+        return user.getUsername();
+    }
 
-	/**
-	 * 获取spel 定义的参数值
-	 * @param context 参数容器
-	 * @param key key
-	 * @param clazz 需要返回的类型
-	 * @param <T> 返回泛型
-	 * @return 参数值
-	 */
-	public <T> T getValue(EvaluationContext context, String key, Class<T> clazz) {
-		SpelExpressionParser spelExpressionParser = new SpelExpressionParser();
-		Expression expression = spelExpressionParser.parseExpression(key);
-		return expression.getValue(context, clazz);
-	}
+    /**
+     * 获取spel 定义的参数值
+     *
+     * @param context 参数容器
+     * @param key     key
+     * @param clazz   需要返回的类型
+     * @param <T>     返回泛型
+     * @return 参数值
+     */
+    public <T> T getValue(EvaluationContext context, String key, Class<T> clazz) {
+        SpelExpressionParser spelExpressionParser = new SpelExpressionParser();
+        Expression expression = spelExpressionParser.parseExpression(key);
+        return expression.getValue(context, clazz);
+    }
 
-	/**
-	 * 获取参数容器
-	 * @param arguments 方法的参数列表
-	 * @param signatureMethod 被执行的方法体
-	 * @return 装载参数的容器
-	 */
-	public EvaluationContext getContext(Object[] arguments, Method signatureMethod) {
-		String[] parameterNames = new LocalVariableTableParameterNameDiscoverer().getParameterNames(signatureMethod);
-		EvaluationContext context = new StandardEvaluationContext();
-		if (parameterNames == null) {
-			return context;
-		}
-		for (int i = 0; i < arguments.length; i++) {
-			context.setVariable(parameterNames[i], arguments[i]);
-		}
-		return context;
-	}
+    /**
+     * 获取参数容器
+     *
+     * @param arguments       方法的参数列表
+     * @param signatureMethod 被执行的方法体
+     * @return 装载参数的容器
+     */
+    public EvaluationContext getContext(Object[] arguments, Method signatureMethod) {
+        String[] parameterNames = new LocalVariableTableParameterNameDiscoverer().getParameterNames(signatureMethod);
+        EvaluationContext context = new StandardEvaluationContext();
+        if (parameterNames == null) {
+            return context;
+        }
+        for (int i = 0; i < arguments.length; i++) {
+            context.setVariable(parameterNames[i], arguments[i]);
+        }
+        return context;
+    }
 
 }
