8d409d637f9896b2462982f826f4a08f31d03570
larry <larryland000@gmail.com>
2023-09-27 02:21:11 +0400

    update

---
+++ model/src/main/java/co/mgentertainment/common/model/player/HeaderConstants.java
@@ -0,0 +1,14 @@
+package co.mgentertainment.common.model.player;
+
+/**
+ * @author larry
+ * @createTime 2023/9/27
+ * @description HeaderConstants
+ */
+public interface HeaderConstants {
+    String HEADER_CLIENT_ID = "clientId";
+    String HEADER_UID = "uid";
+    String HEADER_USERNAME = "username";
+    String HEADER_NICKNAME = "nickname";
+    String HEADER_AVATAR = "avatar";
+}
