e6b063c44854d8820bf3900422e9dab0341b7234
larry <larryland000@gmail.com>
2023-10-15 01:45:09 +0400

    update

--- model/src/main/java/co/mgentertainment/common/model/player/HeaderConstants.java
+++ model/src/main/java/co/mgentertainment/common/model/player/HeaderConstants.java
@@ -12,6 +12,8 @@ public interface HeaderConstants {
     String HEADER_NICKNAME = "_nickname";
     String HEADER_PLATFORM_ID = "_platformId";
     String HEADER_AVATAR = "_avatar";
+    String HEADER_INVITED_CODE = "_invitedCode";
+    String HEADER_PROMOTED_CHANNEL_CODE = "_channelCode";
     String HEADER_REQ_ID = "_requestId";
     String HEADER_REQ_TS = "_requestTimestamp";
     String HEADER_CLIENT_IP = "_clientIp";
