d9aeff920d2e71773a0516336f95b5d462705dac
larry <larryland000@gmail.com>
2023-10-20 22:59:15 +0400

    update

--- common-indicator-spring-boot-starter/src/main/java/co/mgentertainment/common/indicator/config/IndicatorAutoConfiguration.java
+++ common-indicator-spring-boot-starter/src/main/java/co/mgentertainment/common/indicator/config/IndicatorAutoConfiguration.java
@@ -1,6 +1,8 @@
 package co.mgentertainment.common.indicator.config;
 
 import co.mgentertainment.common.indicator.IndicatorCollector;
+import co.mgentertainment.common.indicator.aspect.GlobalIndicatorAspect;
+import co.mgentertainment.common.indicator.aspect.ItemIndicatorAspect;
 import co.mgentertainment.common.redis.service.RedisService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,5 +18,15 @@ public class IndicatorAutoConfiguration {
     @Bean(name = "indicatorCollector")
     public IndicatorCollector indicatorCollector(final RedisService redisService) {
         return new IndicatorCollector(redisService);
+    }
+
+    @Bean(name = "itemIndicatorAspect")
+    public ItemIndicatorAspect itemIndicatorAspect(final RedisService redisService) {
+        return new ItemIndicatorAspect(redisService);
+    }
+
+    @Bean(name = "globalIndicatorAspect")
+    public GlobalIndicatorAspect globalIndicatorAspect(final RedisService redisService) {
+        return new GlobalIndicatorAspect(redisService);
     }
 }
