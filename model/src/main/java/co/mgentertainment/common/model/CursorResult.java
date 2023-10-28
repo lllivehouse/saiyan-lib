c955ffbd6ea2f3c259ac4a0da5c9626fafe3d9df
larry <larryland000@gmail.com>
2023-10-28 13:35:16 +0400

    update

--- model/src/main/java/co/mgentertainment/common/model/CursorResult.java
+++ model/src/main/java/co/mgentertainment/common/model/CursorResult.java
@@ -1,9 +1,11 @@
 package co.mgentertainment.common.model;
 
 import lombok.Data;
+import lombok.ToString;
 
 import java.io.Serializable;
 import java.util.Collections;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -12,30 +14,28 @@ import java.util.List;
  * @description CursorResult
  */
 @Data
+@ToString
 public class CursorResult<T> implements Serializable {
 
     private static final long serialVersionUID = -5983847187357226981L;
 
-    private final boolean hasNext;
     private final List<T> records;
     private final int size;
+    private final Date lastCreateTime;
+    private final boolean hasNext;
 
-    public CursorResult(int pageSize, List<T> rows) {
+    public CursorResult(int pageSize, Date lastCreateTime, List<T> rows) {
         this.size = pageSize;
+        this.lastCreateTime = lastCreateTime;
         this.records = rows;
         this.hasNext = rows.size() == pageSize;
     }
 
-    public static <T> CursorResult<T> createCursorResult(int pageSize, List<T> rows) {
-        return new CursorResult<>(pageSize, rows);
+    public static <T> CursorResult<T> createCursorResult(int pageSize, Date lastCreateTime, List<T> rows) {
+        return new CursorResult<>(pageSize, lastCreateTime, rows);
     }
 
-    public static <T> CursorResult<T> emptyPageResult(int pageSize) {
-        return new CursorResult<>(pageSize, Collections.emptyList());
-    }
-
-    @Override
-    public String toString() {
-        return "CursorResult [hashNext=" + this.hasNext + ", size=" + size + ", records=" + this.records + "]";
+    public static <T> CursorResult<T> emptyPageResult() {
+        return new CursorResult<>(0, null, Collections.emptyList());
     }
 }
