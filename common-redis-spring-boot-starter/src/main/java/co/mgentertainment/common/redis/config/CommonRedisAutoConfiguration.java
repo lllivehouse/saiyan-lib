76850033b672610c70496d13a490019fcf7efc75
larry <larryland000@gmail.com>
2023-11-06 17:28:15 +0400

    update

--- common-redis-spring-boot-starter/src/main/java/co/mgentertainment/common/redis/config/CommonRedisAutoConfiguration.java
+++ common-redis-spring-boot-starter/src/main/java/co/mgentertainment/common/redis/config/CommonRedisAutoConfiguration.java
@@ -2,13 +2,6 @@ package co.mgentertainment.common.redis.config;
 
 import co.mgentertainment.common.redis.service.RedisService;
 import co.mgentertainment.common.redis.service.impl.RedisServiceImpl;
-import co.mgentertainment.common.utils.DateUtils;
-import com.fasterxml.jackson.annotation.JsonAutoDetect;
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.annotation.PropertyAccessor;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.jsontype.BasicPolymorphicTypeValidator;
-import com.fasterxml.jackson.databind.jsontype.impl.LaissezFaireSubTypeValidator;
 import lombok.RequiredArgsConstructor;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.BeansException;
@@ -22,12 +15,7 @@ import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnection;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.StringRedisTemplate;
-import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
-
-import java.text.SimpleDateFormat;
-import java.util.TimeZone;
 
 /**
  * @author larry
@@ -68,46 +56,15 @@ public class CommonRedisAutoConfiguration {
 
     @Bean(name = "redisTemplate")
     public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
-        RedisTemplate<String, Object> template = new RedisTemplate<>();
-        template.setConnectionFactory(redisConnectionFactory);
-
-        ObjectMapper om = new ObjectMapper();
-        // 指定要序列化的域，field,get和set,以及修饰符范围，ANY是都有包括private和public
-        om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
-        om.activateDefaultTyping(BasicPolymorphicTypeValidator.builder().build(),
-                ObjectMapper.DefaultTyping.EVERYTHING);
-        // 指定序列化输入的类型，类必须是非final修饰的，final修饰的类
-        om.activateDefaultTyping(new LaissezFaireSubTypeValidator(), ObjectMapper.DefaultTyping.NON_FINAL);
-        om.setDateFormat(new SimpleDateFormat(DateUtils.DEFAULT_MILLSEC_FORMAT));
-        om.setTimeZone(TimeZone.getTimeZone("GMT+8"));
-        // 不转换值为 null 的对象
-        om.setSerializationInclusion(JsonInclude.Include.NON_NULL);
-
-        Jackson2JsonRedisSerializer<Object> jackson2JsonRedisSerializer =
-                new Jackson2JsonRedisSerializer<>(Object.class);
-        jackson2JsonRedisSerializer.setObjectMapper(om);
-        // key 采用 string 的序列化方式
-        template.setKeySerializer(new StringRedisSerializer());
-        // value 采用 jackson 的序列化方式
-        template.setValueSerializer(jackson2JsonRedisSerializer);
-        // hash 的 key 采用 string 的序列化方式
-        template.setHashKeySerializer(new StringRedisSerializer());
-        // hash 的 value 采用 jackson 的序列化方式
-        template.setHashValueSerializer(jackson2JsonRedisSerializer);
-        template.afterPropertiesSet();
-        return template;
-    }
-
-    @Bean(name = "stringRedisTemplate")
-    public StringRedisTemplate getStringRedisTemplate(RedisConnectionFactory redisConnectionFactory) {
-        StringRedisTemplate template = new StringRedisTemplate(redisConnectionFactory);
-        template.setEnableTransactionSupport(true);
-        template.setValueSerializer(new StringRedisSerializer());
-        template.setKeySerializer(new StringRedisSerializer());
-        template.setHashKeySerializer(new StringRedisSerializer());
-        template.setHashValueSerializer(new StringRedisSerializer());
-        template.afterPropertiesSet();
-        return template;
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+        GsonRedisSerializer gsonRedisSerializer = new GsonRedisSerializer();
+        redisTemplate.setValueSerializer(gsonRedisSerializer);
+        redisTemplate.setHashValueSerializer(gsonRedisSerializer);
+        redisTemplate.afterPropertiesSet();
+        return redisTemplate;
     }
 
     @Bean(name = "redisService")
