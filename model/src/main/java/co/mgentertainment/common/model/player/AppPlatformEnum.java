f371a54dbc1de76e18c191de13f530c1c450cc5b
larry <larryland000@gmail.com>
2023-11-08 19:16:16 +0400

    update

--- model/src/main/java/co/mgentertainment/common/model/player/AppPlatformEnum.java
+++ model/src/main/java/co/mgentertainment/common/model/player/AppPlatformEnum.java
@@ -9,23 +9,30 @@ import java.util.Arrays;
  */
 public enum AppPlatformEnum {
 
-    ANDROID(0),
-    IOS(1),
-    ALL(2),
+    ANDROID(0, "安卓"),
+    IOS(1, "苹果"),
+    ALL(2, "所有"),
     ;
 
     private Integer platformId;
 
-    AppPlatformEnum(Integer platformId) {
+    private String desc;
+
+    AppPlatformEnum(Integer platformId, String desc) {
         this.platformId = platformId;
+        this.desc = desc;
+    }
+
+    public static AppPlatformEnum getByPlatformId(Integer platformId) {
+        return Arrays.stream(values()).filter(e -> e.getPlatformId().equals(platformId)).findFirst().orElse(null);
     }
 
     public Integer getPlatformId() {
         return platformId;
     }
 
-    public static AppPlatformEnum getByPlatformId(Integer platformId) {
-        return Arrays.stream(values()).filter(e -> e.getPlatformId().equals(platformId)).findFirst().orElse(null);
+    public String getDesc() {
+        return desc;
     }
 
     public static AppPlatformEnum getByName(String platformName) {
